e7f0fe18038799970b6e67ee6ef9e637a3285989
James Matsuba <james.matsuba@gmail.com>
2020-03-21 16:35:21 -0700

    adding jquery using yarn

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
+// config/webpack/environment.js
 const { environment } = require('@rails/webpacker')
+
+// From: http://blog.blackninjadojo.com/ruby/rails/2019/03/01/webpack-webpacker-and-modules-oh-my-how-to-add-javascript-to-ruby-on-rails.html
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery" // < I think this could be the line you need.
+  })
+)
 
 module.exports = environment
