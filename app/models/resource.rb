0543fb852af5b1e0e17a1c7662169fb71c874343
Nima Boscarino <nima.boscarino@gmail.com>
2020-03-24 15:57:53 -0700

    "rich_description" instead of "content"

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,8 +2,8 @@ class Resource < ApplicationRecord
 
   belongs_to :category
   belongs_to :user, optional: true
-  has_rich_text :content
-  
+  has_rich_text :rich_description
+
   scope :by_date, -> { order(date: :desc) }
 
 end
