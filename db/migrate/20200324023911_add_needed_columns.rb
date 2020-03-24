8b20599567c910777db245c0395909c6fd269817
James Matsuba <james.matsuba@gmail.com>
2020-03-23 20:33:57 -0700

    updated db schema

---
+++ db/migrate/20200324023911_add_needed_columns.rb
@@ -0,0 +1,12 @@
+class AddNeededColumns < ActiveRecord::Migration[6.0]
+  def change
+    add_column :categories, :kind, :string
+    add_column :categories, :slug, :string
+    add_column :categories, :priority, :integer
+    add_column :resources, :date, :datetime
+    add_column :resources, :description, :string
+    add_column :resources, :video_id, :string
+    add_column :resources, :start_time, :integer
+    change_column :resources, :user_id, :integer, null: true
+  end
+end
