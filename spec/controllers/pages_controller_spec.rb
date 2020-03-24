4054edadf9685638723bef02fb559355b639b2cc
Dennis Marchand <dennis@juul.com>
2020-03-23 18:17:07 -0700

    Very basic requests specs for existing endpoints
    These are more meant to try and keep people from shooting themselves in
    the foot in the future and reduce the amount of manual QA required.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :request do
+  describe '#home' do
+    subject { get '/' }
+
+    it 'responds with a 200 level response code' do
+      subject
+
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe '#resources' do
+    subject { get '/resources' }
+
+    it 'responds with a 200 level response code' do
+      subject
+
+      expect(response.status).to eq(200)
+    end
+  end
+end
