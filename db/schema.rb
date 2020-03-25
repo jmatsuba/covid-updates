8f23e42ec6ad1364cbacdb8be17cea92b1f4bc48
Nima Boscarino <nima.boscarino@gmail.com>
2020-03-25 13:41:02 -0700

    allow nullifying category_id on resource
    If category is deleted, that shouldn't destroy the resource as well.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_225653) do
+ActiveRecord::Schema.define(version: 2020_03_25_203558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_225653) do
     t.string "url"
     t.string "kind"
     t.integer "priority"
-    t.bigint "category_id", null: false
+    t.bigint "category_id"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
